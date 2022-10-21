e647ee16b33a6f4184d8a97d31e8980fabf5900b
Sesdika <sesdika.sansani@gmail.com>
2022-10-21 22:11:11 +0700

    navbar.js - menambahkan right drawer (belum disesuaikan dan di-styling)

--- src/sections/Navbar/navbar.js
+++ src/sections/Navbar/navbar.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import { useState } from 'react';
 import { Container } from '@mui/material';
 import {DehazeRounded, SearchRounded} from "@mui/icons-material";
 import logo from '../../assets/logo.png';
 import {Link, NavLink, Outlet, useNavigate} from "react-router-dom";
 import navbar from "./navbar.css";
+// import RightDrawer from "./RightDrawer";
+
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 function Navbar(props) {
+    const [stateOpen, setStateOpen] = useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        // setState({ ...state, [anchor]: open });
+        setStateOpen(open);
+    };
+
+    const list = () => (
+        <Box
+          sx={{ width: 1/2 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
     return (
         <div>
             <nav>
@@ -20,6 +83,7 @@ function Navbar(props) {
                     </div>
                     <NavLink to={"about"}>About us</NavLink>
                     <NavLink to={"channels"}>Channel</NavLink>
+                    <Button onClick={toggleDrawer(true)}>Drawer</Button>
                 </div>
                 <div id={"navbar-section-right"}>
                     <SearchRounded/>
@@ -29,6 +93,14 @@ function Navbar(props) {
             <main>
                 <Outlet />
             </main>
+            <SwipeableDrawer
+                anchor='right'
+                open={stateOpen}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+                >
+                {list()}
+            </SwipeableDrawer>
         </div>
     );
 }
